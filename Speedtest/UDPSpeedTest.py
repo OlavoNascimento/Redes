35cee31eb7a31f1d6feb87ef74623f04f77ffe7b
Olavo Nascimento <olavo.nascimento4@gmail.com>
2022-01-23 18:45:45 -0300

    Implementar classe para teste de conexão utilizando socket UDP.

---
+++ Speedtest/UDPSpeedTest.py
@@ -0,0 +1,138 @@
+import logging
+from datetime import datetime, timedelta
+from socket import timeout
+
+from tqdm import tqdm
+
+from client import Client, Roles, SocketType
+
+
+class UDPTester(Client):
+    """
+    Classe responsável por transmitir dados utilizando sockets UDP e calcular as velocidades de
+    download e upload entre dois computadores.
+    """
+
+    # Pacote utilizado para confirmar que o outro usuário recebeu um pacote.
+    CONFIRMATION_PACKET = b"\x01"
+
+    def __init__(
+        self,
+        connect_address: str,
+        port: int,
+        starting_role: Roles,
+    ):
+        super().__init__(
+            connect_address,
+            port,
+            starting_role,
+            SocketType.UDP,
+        )
+
+    def receive_data(self) -> int:
+        """
+        Recebe dados enviados por outro usuário, armazenando o número de bytes recebidos, ao final
+        da transmissão envia o total recebido para o outro usuário.
+        """
+        self.connection.settimeout(1)
+
+        waiting_message_presented = False
+        pbar = None
+        received_data_size = 0
+
+        next_tick = datetime.now() + timedelta(seconds=1)
+        while True:
+            try:
+                data = self.connection.recv(self.PACKET_SIZE)
+                logging.debug(
+                    "%d bytes recebidos, tamanho atual: %d", len(data), received_data_size
+                )
+                # Um pacote vazio indica o fim da transmissão de dados.
+                if data == self.EMPTY_PACKET:
+                    break
+                if received_data_size == 0:
+                    print("Testando velocidade de download...")
+                    pbar = tqdm(total=self.RUN_DURATION, bar_format=self.TQDM_FORMAT)
+                received_data_size += len(data)
+
+                current_time = datetime.now()
+                # Atualiza a barra de progresso.
+                if current_time >= next_tick:
+                    pbar.update(1)
+                    pbar.refresh()
+                    next_tick = current_time + timedelta(seconds=1)
+            except timeout:
+                # Indica que está pronto para receber dados.
+                self.connection.send(self.EMPTY_PACKET)
+            except ConnectionRefusedError:
+                # Outro usuário ainda não se conectou.
+                if not waiting_message_presented:
+                    print("Esperando o outro usuário estabelecer uma conexão...")
+                    waiting_message_presented = True
+        logging.debug("Fim do recebimento de dados")
+
+        # Garante que o tempo de execução da barra de progresso esteja correto ao fina da
+        # execução.
+        pbar.n = self.RUN_DURATION
+        pbar.refresh()
+        pbar.close()
+
+        # Envia o total salvo para o outro usuário.
+        while True:
+            try:
+                self.connection.send(int.to_bytes(received_data_size, 8, "big", signed=False))
+                status = self.connection.recv(self.PACKET_SIZE)
+                if status == self.CONFIRMATION_PACKET:
+                    break
+            except timeout:
+                pass
+            except ConnectionRefusedError:
+                break
+
+        return received_data_size
+
+    def send_data(self):
+        """
+        Envia dados para outro usuário, ao final da transmissão recebe o total de bytes recebidos
+        pelo o outro usuário.
+        """
+        # Uma mensagem vazia indica que o outro usuário está pronto para receber os dados.
+        print("Esperando o outro usuário estabelecer uma conexão...")
+        _, address = self.connection.recvfrom(self.PACKET_SIZE)
+        print("Testando velocidade de upload...")
+
+        end_time = datetime.now() + timedelta(seconds=self.RUN_DURATION)
+        next_tick = datetime.now() + timedelta(seconds=1)
+
+        logging.debug("Iniciando envio de dados")
+        with tqdm(total=self.RUN_DURATION, bar_format=self.TQDM_FORMAT) as pbar:
+            while (current_time := datetime.now()) < end_time:
+                self.connection.sendto(self.data, address)
+                # Atualiza a barra de progresso.
+                if current_time >= next_tick:
+                    next_tick = current_time + timedelta(seconds=1)
+                    pbar.update(1)
+                    pbar.refresh()
+
+            # Garante que o tempo de execução da barra de progresso esteja correto ao fina da
+            # execução.
+            pbar.n = self.RUN_DURATION
+            pbar.refresh()
+        logging.debug("Fim do envio de dados")
+
+        self.connection.settimeout(1)
+        bytes_transmitted = 0
+        # Recebe o total de bytes recebidos pelo o outro usuário.
+        while bytes_transmitted == 0:
+            try:
+                self.connection.sendto(self.EMPTY_PACKET, address)
+                message_end = self.connection.recv(self.PACKET_SIZE)
+                if len(message_end) == 8:
+                    bytes_transmitted = int.from_bytes(message_end, "big", signed=False)
+                    self.connection.sendto(self.CONFIRMATION_PACKET, address)
+            except timeout:
+                pass
+        self.connection.settimeout(0)
+        logging.debug("%d bytes foram recebidos pelo o outro usuário", bytes_transmitted)
+
+        return bytes_transmitted
